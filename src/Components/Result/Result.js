2f61424a7b298165e77333fb6bc92da63aa77113
sabbirahmedseam <sabbirahmedseam1@gmail.com>
2022-09-29 10:10:19 +0600

    send

--- src/Components/Result/Result.js
+++ src/Components/Result/Result.js
@@ -1,22 +1,34 @@
-
 import "./Result.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import logo from "../../man.jpg";
 
-const Result = ( {show,sum,timeClick}) => {
-    // console.log(sum);
-const diffToast=()=>{
-  alert('ok');
-}
+const Result = ({ show, sum, timeClick }) => {
+  // console.log(sum);
+  const diffToast = () => {
+    toast("Wow so easy!");
+  };
   return (
     <div>
       <div className="result">
+        <div className="man_name">
+          <img className="man" src={logo} alt="" />
+          <div>
+          <h3>Hasin Kamal</h3>
+          <p>Mirpur,Bangladesh</p>
+          </div>
+        </div>
+<div>
+  
+</div>
         <h1>result</h1>
         <h3>Add A Break</h3>
         <div className="second">
-          <span onClick={()=>timeClick('10s')}>10s</span>
-          <span onClick={()=>timeClick('20s')} >20s</span>
-          <span onClick={()=>timeClick('30s')}>30s</span>
-          <span onClick={()=>timeClick('40s')}>40s</span>
-          <span onClick={()=>timeClick('50s')}>50s</span>
+          <span onClick={() => timeClick("10s")}>10s</span>
+          <span onClick={() => timeClick("20s")}>20s</span>
+          <span onClick={() => timeClick("30s")}>30s</span>
+          <span onClick={() => timeClick("40s")}>40s</span>
+          <span onClick={() => timeClick("50s")}>50s</span>
         </div>
         <div className="details">
           <h3>Exercise Details</h3>
@@ -28,9 +40,12 @@ const diffToast=()=>{
             Break time <span>{show}</span>
           </b>
         </div>
-        <button onClick={diffToast}>
-          <p>Activity Completed</p>
-        </button>
+        <div>
+          <button onClick={diffToast}>
+            <p>Activity Completed</p>
+          </button>
+        </div>
+        <ToastContainer />
       </div>
     </div>
   );
