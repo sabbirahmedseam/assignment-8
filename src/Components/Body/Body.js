d0b7156b9d85d9c9a52b851bb55acbc9b59ded4c
sabbirahmedseam <sabbirahmedseam1@gmail.com>
2022-09-29 12:14:20 +0600

    send

--- src/Components/Body/Body.js
+++ src/Components/Body/Body.js
@@ -4,35 +4,24 @@ import Man from "../Man/Man";
 
 import "./Body.css";
 
-const Body = ({list}) => {
+const Body = ({ list }) => {
   const [body, setBody] = useState([]);
   useEffect(() => {
     fetch("products.json")
       .then((res) => res.json())
       .then((data) => setBody(data));
   }, []);
- 
+
   return (
-   <div>
-    <h1 className="boost">BOOST-ACTIVE-CLUB</h1>
-    <h4>Select today's exercise</h4>
-     <div className='body_part'>
-    
-    {body.map((item) =>(
-      <Man
-     list={list}
-    key={item.id}
-      items={item}
-      >
-
-      </Man>
-    )
-
-    <p></p>
-    
-    )}
-  </div>
-   </div>
+    <div>
+      <h1 className="boost">BOOST-ACTIVE-CLUB</h1>
+      <h4>Select today's exercise</h4>
+      <div className="body_part">
+        {body.map((item) => (
+          <Man list={list} key={item.id} items={item}></Man>
+        ))}
+      </div>
+    </div>
   );
 };
 
